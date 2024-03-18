ebc041ef48c4707b7b0fc93f062c874099521d92
christiechindy <chindychristie@gmail.com>
2024-03-18 11:08:01 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FileIcon from "@/Icons/FileIcon";
 import PdfIcon from "@/Icons/PdfIcon";
+import UploadIcon from "@/Icons/UploadIcon";
 import XIcon from "@/Icons/XIcon";
 import { useEffect, useState } from "react";
 import React from "react";
@@ -28,6 +29,10 @@ const page = () => {
     const cancelHandler = () => {
         setFile(undefined);
     }
+
+    /* --- Form ------------------ */
+    const [terlangkah, setTerlangkah] = useState<boolean|undefined>();
+    const [stepOnMiddle, setStepOnMiddle] = useState<boolean|undefined>();
 
     useEffect(() => {
         if (file) {
@@ -60,25 +65,67 @@ const page = () => {
                             <div className="box">
                                 <div className="topbar">
                                     <div className="filename">
-                                        <FileIcon/> <div>{file?.name}</div>
+                                        <FileIcon/> <div>{file?.name?.length > 40 ? `${file?.name?.substring(0,40)}....pdf` : file?.name}</div>
                                     </div>
                                     <div className="cancel" onMouseOver={() => setXOver(true)} onMouseLeave={() => setXOver(false)} onClick={cancelHandler}><XIcon color={XOver ? "#ff0000" : "#D41818"} /></div>
                                 </div>
                                 <div className="fields">
                                     <div className="row">
-                                        <label htmlFor="halaman">Halaman</label>
-                                        <div className="halamanInp">
-                                            <input type="text" />
-                                            <div>-</div>
-                                            <input type="text" />
+                                        <label htmlFor="judulBuku">Judul Buku</label>
+                                        <input type="text" id="judulBuku" className="input" />
+                                    </div>
+                                    <div className="row">
+                                        <label className="label_lihe">Apakah ada nomor halaman yang terlangkah?</label>
+                                        <div className="onerow">
+                                            <div className="radio">
+                                                <input type="radio" name="terlangkah" value="ya" id="ya" onChange={() => setTerlangkah(true)} checked={terlangkah===true} />
+                                                <label htmlFor="ya">Ya</label>
+                                            </div>
+                                            <div className="radio">
+                                                <input type="radio" name="terlangkah" value="tidak" id="tidak" onChange={() => setTerlangkah(false)} checked={terlangkah===false} />
+                                                <label htmlFor="tidak">Tidak</label>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="row">
-                                        <label htmlFor="judul">Judul Buku</label>
-                                        <input type="text" />
+                                        <label className="label_lihe">Apakah ada halaman tengah yang tidak ingin diindeks?</label>
+                                        <div className="onerow">
+                                            <div className="radio">
+                                                <input type="radio" name="stepOnMiddle" value="ya" id="ya_" onChange={() => setStepOnMiddle(true)} checked={stepOnMiddle===true} />
+                                                <label htmlFor="ya_">Ya</label>
+                                            </div>
+                                            <div className="radio">
+                                                <input type="radio" name="stepOnMiddle" value="tidak" id="tidak_" onChange={() => setStepOnMiddle(false)} checked={stepOnMiddle===false} />
+                                                <label htmlFor="tidak_">Tidak</label>
+                                            </div>
+                                        </div>
                                     </div>
+                                    {(terlangkah!==undefined && stepOnMiddle!==undefined) ?
+                                        terlangkah===false && stepOnMiddle===false ?
+                                            <div className="row">
+                                                <label htmlFor="halaman">Halaman yang ingin dibuatkan indeks</label>
+                                                <div className="halamanInp">
+                                                    <input type="number" className="input" />
+                                                    <div>-</div>
+                                                    <input type="number" className="input" />
+                                                </div>
+                                            </div>
+                                        :
+                                            <div className="row">
+                                                <label htmlFor="jsonfile" className="label_lihe">Unggah JSON file berisi range halaman tiap bab/section dan selisih nomor halaman yang dilabeli dan urutan halaman keseluruhan</label>
+                                                <a href="/halaman.json" download="halaman.json" className="linkjsonformat">(download format JSON file)</a>
+                                                <input type="file" id="jsonfile" />
+                                                <label htmlFor="jsonfile" className="input file">
+                                                    <UploadIcon />
+                                                    <div>Unggah file json yang sudah diisi</div>
+                                                </label>
+                                            </div>
+                                    : null}
+                                    
                                 </div>
                             </div>
+                            
+                            <button className="processBtn">Proses</button>
                         </div>
                     </div>
                 }
